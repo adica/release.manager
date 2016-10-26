1fbef268ef7fc9328003b7471b639e579ce078fe
adica <carmeladi@gmail.com>
2016-10-26 10:01:00 +0300

    add API support

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -9,6 +9,7 @@ import AppBar from './AppBar.js';
 //import Search from './Search.js';
 //import ApplicationSelect from './ApplicationSelect.js';
 import ListItem from './ListItem.js';
+import api from './../stores/api.js';
 
 class AppComponent extends React.Component {
 
@@ -16,45 +17,8 @@ class AppComponent extends React.Component {
     return { muiTheme: getMuiTheme(baseTheme) };
   }
   render(){
-    const appList = [
-      { 
-        'key' : 'sarine.viewer.templates.widget',
-        'val' : 'BASEWIDGET',
-        'desc' : 'Base Widget',
-        'icon' : 'Roundicons-22.svg'
-      },
-      { 
-        'key' : 'sarine.viewer.templates.dashboard', 
-        'val' : 'BASEDASHBOARD',
-        'desc' : 'Base Dashboard',
-        'icon' : 'Roundicons-24.svg'
-      },
-      { 
-        'key' : 'sarine.viewer.3dfullinspection', 
-        'val' : '3D-ATOM',
-        'desc' : '3D Atom',
-        'icon' : 'Roundicons-30.svg'
-      },
-      { 
-        'key' : 'sarine.viewer.templates.jewelry', 
-        'val' : 'BASEJEWELRY',
-        'desc' : 'Base Jewelry',
-        'icon' : 'Roundicons-54.svg'
-      },
-      { 
-        'key' : 'sarine.viewer.core', 
-        'val' : 'CORE',
-        'desc' : 'Viewer Core',
-        'icon' : 'Roundicons-55.svg'
-      },
-      { 
-        'key' : 'sarine.viewer.templates.fullscreen', 
-        'val' : 'FULLSCREEN',
-        'desc' : 'Base Full Screen',
-        'icon' : 'Roundicons-60.svg'
-      }
-   ];
-    const envList = [{ 'key' : 'qa4', 'val' : 'QA4'},{ 'key' : 'stg1', 'val' : 'STG'},{ 'key' : 'prod1', 'val' : 'PROD'}]
+    const appList = api.apps;
+    const envList = api.envs;
       return (
     	<div>
     		<AppBar />
--- client/src/stores/api.js
+++ client/src/stores/api.js
@@ -10,4 +10,49 @@ exports.search = (searchVal) => {
 	{'name' : 'Session Storage', 'value': '/src/client/storage.js'}];
 }
 
+exports.envs = [
+	{ 'key' : 'qa4', 'val' : 'QA4'},
+	{ 'key' : 'stg1', 'val' : 'STG'},
+	{ 'key' : 'prod1', 'val' : 'PROD'}
+];
+
+exports.apps = [
+      { 
+        'key' : 'sarine.viewer.templates.widget',
+        'val' : 'BASEWIDGET',
+        'desc' : 'Base Widget',
+        'icon' : 'Roundicons-22.svg'
+      },
+      { 
+        'key' : 'sarine.viewer.templates.dashboard', 
+        'val' : 'BASEDASHBOARD',
+        'desc' : 'Base Dashboard',
+        'icon' : 'Roundicons-24.svg'
+      },
+      { 
+        'key' : 'sarine.viewer.3dfullinspection', 
+        'val' : '3D-ATOM',
+        'desc' : '3D Atom',
+        'icon' : 'Roundicons-30.svg'
+      },
+      { 
+        'key' : 'sarine.viewer.templates.jewelry', 
+        'val' : 'BASEJEWELRY',
+        'desc' : 'Base Jewelry',
+        'icon' : 'Roundicons-54.svg'
+      },
+      { 
+        'key' : 'sarine.viewer.core', 
+        'val' : 'CORE',
+        'desc' : 'Viewer Core',
+        'icon' : 'Roundicons-55.svg'
+      },
+      { 
+        'key' : 'sarine.viewer.templates.fullscreen', 
+        'val' : 'FULLSCREEN',
+        'desc' : 'Base Full Screen',
+        'icon' : 'Roundicons-60.svg'
+      }
+   ];
+
 
